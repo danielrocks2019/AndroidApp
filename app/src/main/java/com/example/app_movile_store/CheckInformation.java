c9ef659c3ee14ff3f4c7daa25c2d6138b3196f23
amilkar miguel <amcmiki@gmail.com>
2019-06-23 21:32:32 -0400

    Initial commit

---
+++ app/src/main/java/com/example/app_movile_store/CheckInformation.java
@@ -0,0 +1,4 @@
+package com.example.app_movile_store;
+
+class CheckInformation {
+}
---
+++ app/src/main/java/com/example/app_movile_store/Host/host.java
@@ -0,0 +1,16 @@
+package com.example.app_movile_store.Host;
+
+public class host {
+
+
+    // public String ip="http://192.168.43.174";
+
+    public String ip="http://192.168.42.200";
+//192.168.1.6
+
+
+    public String getIp() {
+        return this.ip;
+    }
+
+}
