34dcd048cf0dc44b6c6b42d68b49046a5da1e113
Daniel Cruz <leinadrocks_2019_@hotmail.com>
2019-07-16 22:17:53 -0400

    servicios v1

---
+++ app/src/main/java/com/example/app_movile_store/DATAPRO/DataApp.java
@@ -0,0 +1,11 @@
+package com.example.danielrocks.servicios.DATAPRO;
+
+import com.example.danielrocks.servicios.ItemMenuPro.ItemMenuStructure;
+
+import java.util.ArrayList;
+
+public class DataApp {
+    static public ArrayList<ItemMenuStructure>LISTDATA;
+    static public String HOST = "http://192.168.43.72:8000";
+    //static public ArrayList<ItemList> LISTINFO;
+}
