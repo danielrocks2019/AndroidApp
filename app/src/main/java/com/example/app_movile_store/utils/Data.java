932387af6021d67e20ca0e14f181cb42b2649e21
Daniel Cruz <leinadrocks_2019_@hotmail.com>
2019-07-16 23:18:48 -0400

    Administrador

---
+++ app/src/main/java/com/example/app_movile_store/utils/Data.java
@@ -0,0 +1,21 @@
+package com.example.app_movile_store.utils;
+
+public class Data {
+
+
+    public static String TOKEN = "Data eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiZGVpdmlkMTM0NTZAZ21haWwuY29tIiwicGFzc3dvcmQiOiIxMiIsImlhdCI6MTU0NDIyMDMzOX0.SrZWEwHM0BUNp5o98g0fipyX5x7SRcCXr_dkfCMrnYs";
+    public static String FOTO="http://192.168.43.72:7777";//wifi ip 192.168.1.116
+    public static  String  HOST  = "http://192.168.43.72:7777/api/v1.0";  //wifi ip 192.168.1.116
+    public static  String  REGISTER_TIENDA = HOST + "/tienda";
+    public static String UPLOAD_TIENDA = HOST + "/uploadtienda";
+    public static  String  REGISTER_MENUS = HOST + "/menus";
+    public static  String  REGISTER_CLIENTE = HOST + "/cliente";
+    public static  String  REGISTER_LOGIN = HOST + "/login";
+    public static String ID_User="0";
+    public static String ID_TIENDA="0";
+
+    public static String ID_Menus = "0";
+    public  static String Tipo="";
+
+
+}
